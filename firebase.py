b44d2dfd29d90129e67abfcabd6dd862c1ace018
Aaryan Gulia <aaryangulia@Aaryans-MacBook-Pro.local>
2021-03-28 20:43:44 +0300

    kinda..

--- firebase.py
+++ firebase.py
@@ -1,8 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import requests
-import json
 import string
+from operator import itemgetter
 
 cred = credentials.Certificate("uni-hub-c04bd-firebase-adminsdk-5sbqj-ba8cc08404.json")
 firebase_admin.initialize_app(cred)
@@ -11,7 +10,7 @@ db = firebase_admin.firestore.client()
 class firebase():
 
     def signup(self, email, password):
-        new_user = db.collection("users").document().set({'email':email, 'password':password})
+        new_user = db.collection("users").document().set({'email':email.lower(), 'password':password})
 
     def signin(self, email, password):
         self.email = email
@@ -32,3 +31,22 @@ class firebase():
         info["user_id"] = self.user_id
         new_activity = db.collection("Activities").document().set(info)
         return True
+
+    def search_activity(self,info):
+        self.activities = list()
+        activities = db.collection('Activities').stream()
+        for activity in activities:
+            match_score = 0
+            activity_dict = activity.to_dict()
+            if (activity_dict['name'].lower() == info['name'].lower()) and len(info['name']) != 0:
+                match_score += 1
+            if (activity_dict['location'].lower() == info['location'].lower()) and len(info['location']) != 0:
+                match_score += 2
+            if (activity_dict['remote/in_person']==info['remote/in_person']):
+                match_score += 1
+            self.activities.append([activity_dict,match_score])
+        print(self.activities)
+        return self.activities
+
+    def get_activities(self):
+        return self.activities
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from kivy.uix.floatlayout import FloatLayout #helps create a floating lay out
 from kivy.uix.scatter import Scatter #helps move widgets in any manner with hand when app is running
 from kivy.uix.textinput import TextInput #allows user to input text
 from kivy.uix.boxlayout import BoxLayout #arranges all child widgets in a box layout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.uix.checkbox import CheckBox
@@ -13,8 +15,10 @@ from kivy.lang import Builder #to include Kv file without issues with name.
 from kivy.properties import ListProperty, ObjectProperty, NumericProperty #to declare non-kivy entities as kivy properties
 from kivy.graphics.vertex_instructions import (Rectangle, Ellipse, Line) #canvas graphics
 from kivy.graphics.context_instructions import Color #canvas graphics
+
 from functools import partial
 from os import walk
+import time
 
 from firebase import firebase
 
@@ -28,17 +32,43 @@ class JoinActivityScreen(Screen):
     pass
 class ActivitySubmittedScreen(Screen):
     pass
+class SearchResultScreen(Screen):
+    def CreateButtons(self):
+        activities=MainApp.fb.get_activities()
+        for i in range(len(activities)-1):
+            button = Button(text = (activities[i][0]['name'])+'\n'+'Location: '+(activities[i][0]['location'])+'\n'+'remote/in person: '+str(activities[i][0]['remote/in_person'][0])+'/'+str(activities[i][0]['remote/in_person'][1])+'\n'+'Date: '+(activities[i][0]['date'])+'\n'+'Time: '+(activities[i][0]['time'])+'\n'+'Discription: '+(activities[i][0]['discription'])+'\n'+'Contact Creator: '+(activities[i][0]['email']))
+            button.id = str(i)
+            button.background_normal="pictures/bgtexture2.jpg"
+            button.background_down="pictures/bgtexture2.jpg"
+            button.color=(1,1,1)
+            button.font_size=30
+            button.size_hint=(1.0,0.15)
+            button.pos_hint={'top':0.75-2*i/10,'right':1.0}
+            button.halign='center'
+            self.ids['scroll_gridlayout'].size_hint_y = 10*i/10
+            self.ids['scroll_gridlayout'].add_widget(button)
+
 class ProfileScreen(Screen):
+    pass
+class ActivityScreen(GridLayout):
+    activity_dict = ObjectProperty(None)
     pass
 
 class MainApp(App):
-    def build(self):
-        self.fb = firebase()
+    fb = firebase()
 
     def change_screen(self,screen_name):
         print(self.root)
         screen_changer = self.root
         screen_changer.current = screen_name
+
+    def new_activity_screen(self,i):
+        activity = MainApp.fb.get_activities()[i][1]
+        name = activity['name']+str(time.time())
+        id = name
+        s = ActivityScreen(name=name,id=id,activity_dict=activity)
+        self.root.add_widget(s)
+        self.root.current = s
 
 if __name__=='__main__':
     MainApp().run()
